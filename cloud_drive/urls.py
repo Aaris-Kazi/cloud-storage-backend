998e82384df16641a146ef3700fc4daf48a5cb45
Aaris-Kazi <aariskazi@gmail.com>
2025-07-21 00:39:11 +0530

    UPDATE: this will allow to preview images

--- cloud_drive/urls.py
+++ cloud_drive/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('health_app.urls')),
     path('auth/', include('authentication.urls')),
     path('api/v1/', include('file_system.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
